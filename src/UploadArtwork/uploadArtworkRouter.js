f8a2bdedbe6aadb84d0ce199af1695b8f6c08c71
Logan Ellis <mrloganellis@gmail.com>
2020-03-16 13:42:08 -0400

    in the process of trying to create links for images to upload to thge database

--- src/UploadArtwork/uploadArtworkRouter.js
+++ src/UploadArtwork/uploadArtworkRouter.js
@@ -1,14 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const uuid = require('uuid/v4')
 const logger = require('../logger')
 const artworkData = require('../artworkData')
 const xss = require('xss')
 const UploadArtwork = require('./uploadArtworkService')
+const cloudinary = require('cloudinary')
+const formData = require('express-form-data')
 
+const app=express();
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+})
+
+const bodyParser = formData.parse();
 
 const uploadArtworkRouter = express.Router()
-const bodyParser = express.json()
 
 const serializeArtwork = artwork => ({
     id: artwork.id,
@@ -19,37 +29,53 @@ const serializeArtwork = artwork => ({
     description: xss(artwork.description),
 })
 
+// const path = require("path");
+// const multer = require("multer");
+
+// const storage = multer.diskStorage({
+//    destination: "./public/uploads/",
+//    filename: function(req, file, cb){
+//       cb(null,"IMAGE-" + Date.now() + path.extname(file.originalname));
+//    }
+// });
+
+// const upload = multer({
+//    storage: storage,
+//    limits:{fileSize: 1000000},
+// }).single("myImage");
 
 uploadArtworkRouter
     .route('/api/uploadArtwork')
     .post(bodyParser, (req, res) => {
-        for (const field of ['image', 'title', 'description']) {
-            if (!req.body[field]) {
-                logger.error(`${field} is required`)
-                return res  
-                    .status(400)
-                    .send(`'${field}' is required`)
-            }
-        }
 
-        const {image, title, artist_name, price, description} = req.body
+        const {title, artist_name, price, description} = req.body
 
-        const artwork = {id: uuid(), image, title, artist_name, price, description}
+        
+        const values = Object.values(req.files)
+        const image = values.map(image => cloudinary.uploader.upload(image.path))
         const uploadedArtwork = {image, title, artist_name, price, description}
 
-        UploadArtwork.insertArtwork(
-            req.app.get('db'),
-            uploadedArtwork
-        )
+            UploadArtwork.insertArtwork(
+                req.app.get('db'),
+                uploadedArtwork
+            )
+
+            if(!err)
+                return res
+                    .status(201)
+                    .location(`http://localhost:8000/api/uploadArtwork${artwork.id}`)
+                    .json(serializeArtwork(artwork))
+
+        
+
+        const artwork = {id: uuid(), image, title, artist_name, price, description}
+
 
         artworkData.artwork.push(artwork)
 
         logger.info(`Artwork with id ${artwork.id} is uploaded!`)
-        res
-            .status(201)
-            .location(`http://localhost:8000/api/uploadArtwork${artwork.id}`)
-            .json(serializeArtwork(artwork))
-
+        
     })
+
 
 module.exports = uploadArtworkRouter
