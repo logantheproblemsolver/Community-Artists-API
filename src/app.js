e86e42461f12f96582935339a3c685cf15a01997
Logan Ellis <mrloganellis@gmail.com>
2020-03-18 13:23:58 -0400

    added to allow cross domain

--- src/app.js
+++ src/app.js
@@ -18,6 +18,14 @@ const morganOption = (NODE_ENV === 'production')
     app.use(morgan(morganOption))
     app.use(helmet())
     app.use( cors())
+    var allowCrossDomain = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Methods', 'GET,POST');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    };
+    
+    app.use(allowCrossDomain)
 
 
     app.get('/api', (req, res) => {
