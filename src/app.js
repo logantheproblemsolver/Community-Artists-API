292a4d06a5432f9dd3c420efc74b62435c6a4800
unknown <mrloganellis@gmail.com>
2020-03-22 11:05:48 -0400

    fixed styling issues

--- src/app.js
+++ src/app.js
@@ -1,48 +1,44 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require ('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const {NODE_ENV, CLIENT_ORIGIN} = require('./config')
-const logger = require('./logger')
-const showArtworkRouter = require('./ShowArtwork/showArtworkRouter')
-const uploadArtworkRouter = require('./UploadArtwork/uploadArtworkRouter')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const logger = require('./logger');
+const showArtworkRouter = require('./ShowArtwork/showArtworkRouter');
+const uploadArtworkRouter = require('./UploadArtwork/uploadArtworkRouter');
 
-const app = express()
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+  ? 'tiny'
+  : 'common';
 
 
-
-    app.use(morgan(morganOption))
-    app.use(helmet())
-    app.use(cors());    
-
-    
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 
-    app.get('/api', (req, res) => {
-        res.json({ok: true})
-    })
+app.get('/api', (req, res) => {
+  res.json({ ok: true });
+});
 
-    app.use(function errorHandler(error, req, res, next) {
-        console.error(error)
+function errorHandler(error, res) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    logger.error(error.message);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+}
 
-        let response 
-        if (NODE_ENV === 'production') {
-            response = {error: {message: 'server error'}}
-        } else {
-            console.error(error)
-            logger.error(error.message)
-            response = {message: error.message, error}
-        }
-        res.status(500).json(response)
-    })
+app.use(errorHandler());
 
-    app.use(showArtworkRouter)
-    app.use(uploadArtworkRouter)
+app.use(showArtworkRouter);
+app.use(uploadArtworkRouter);
 
 
-    module.exports = app
+module.exports = app;
--- src/artworkData.js
+++ src/artworkData.js
@@ -1,15 +1,15 @@
-const uuid = require('uuid/v4')
+const uuid = require('uuid/v4');
 
 
 const artwork = [
-    {
-        id: uuid(),
-        image: 'https://res.cloudinary.com/nerdmagic/image/upload/v1584385951/pknj5pedpb8v4uwsjddx.jpg',
-        title: 'Absract Art', 
-        artist_name: 'Unknown',
-        price: '$0',
-        description: 'Just a random I got off of Google'
-    },
-]
+  {
+    id: uuid(),
+    image: 'https://res.cloudinary.com/nerdmagic/image/upload/v1584385951/pknj5pedpb8v4uwsjddx.jpg',
+    title: 'Absract Art',
+    artist_name: 'Unknown',
+    price: '$0',
+    description: 'Just a random I got off of Google',
+  },
+];
 
-module.exports = {artwork}
+module.exports = { artwork };
